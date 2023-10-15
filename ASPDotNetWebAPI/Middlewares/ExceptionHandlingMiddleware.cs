eaa2f5cc240ceddd10b4531f6e83fcd78fa93241
Vitaliy Ivanov <ivanoff4123@gmail.com>
2023-10-16 00:42:43 +0700

    Added middleware for exception handling [#18894]

---
+++ ASPDotNetWebAPI/Middlewares/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,50 @@
+﻿using ASPDotNetWebAPI.Models.DTO;
+using System.Net;
+using System.Text.Json;
+
+namespace ASPDotNetWebAPI.Middlewares
+{
+    public class ExceptionHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionHandlingMiddleware> _logger;
+
+        public ExceptionHandlingMiddleware(RequestDelegate next, ILogger<ExceptionHandlingMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task TaskAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        private async Task HandlExceptionAsync(HttpContext httpContext, string exceptionMessage, HttpStatusCode httpStatusCode, string message)
+        {
+            _logger.LogError(exceptionMessage);
+
+            HttpResponse response = httpContext.Response;
+
+            response.ContentType = "application/json";
+            response.StatusCode = (int)httpStatusCode;
+
+            ErrorDTO errorDTO = new()
+            {
+                Message = message,
+                Status = (int)httpStatusCode
+            };
+
+            string result = JsonSerializer.Serialize(errorDTO);
+
+            await response.WriteAsync(result);
+        }
+    }
+}
--- ASPDotNetWebAPI/Program.cs
+++ ASPDotNetWebAPI/Program.cs
@@ -1,3 +1,4 @@
+using ASPDotNetWebAPI.Middlewares;
 using ASPDotNetWebAPI.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -25,6 +26,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseMiddleware<ExceptionHandlingMiddleware>();  
 
 app.UseHttpsRedirection();
 
