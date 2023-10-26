3de6fa4d882251393592876f69afbdfad2913009
Vitaliy Ivanov <ivanoff4123@gmail.com>
2023-10-27 00:35:39 +0700

    Add model DishBasketDTO and BasketService [#20504]
    Added a new model, also created a service for working with dishes in the basket.

---
+++ ASPDotNetWebAPI/Services/BasketService.cs
@@ -0,0 +1,30 @@
+﻿using ASPDotNetWebAPI.Models;
+using ASPDotNetWebAPI.Models.DTO;
+
+namespace ASPDotNetWebAPI.Services
+{
+    public class BasketService : IBasketService
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public BasketService(ApplicationDbContext dbContext) 
+        {
+            _dbContext = dbContext;
+        }
+
+        public Task<List<DishBasketDTO>> GetDishInBasket(Guid userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task PutDishInBasket(Guid userId, Guid dishId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveDishFromBasket(Guid userId, Guid dishId, bool increase)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ASPDotNetWebAPI/Services/IBasketService.cs
@@ -0,0 +1,11 @@
+﻿using ASPDotNetWebAPI.Models.DTO;
+
+namespace ASPDotNetWebAPI.Services
+{
+    public interface IBasketService
+    {
+        Task<List<DishBasketDTO>> GetDishInBasket(Guid userId);
+        Task PutDishInBasket(Guid userId, Guid dishId);
+        Task RemoveDishFromBasket(Guid userId, Guid dishId, bool increase);
+    }
+}
