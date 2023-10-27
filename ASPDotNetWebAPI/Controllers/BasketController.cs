1f980ff653a1bcbf58c3b7d5f84704383cb372a3
Vitaliy Ivanov <ivanoff4123@gmail.com>
2023-10-27 13:47:03 +0700

    Add BasketController [#20505]
    A new BasketController controller was added, corresponding methods were created for it according to the task, routing to methods and the CustomAuthorize attribute were also added.

---
+++ ASPDotNetWebAPI/Controllers/BasketController.cs
@@ -0,0 +1,40 @@
+﻿using ASPDotNetWebAPI.CustomValidationAttributes;
+using ASPDotNetWebAPI.Helpers;
+using ASPDotNetWebAPI.Models.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPDotNetWebAPI.Controllers
+{
+    [Route("api/basket")]
+    [ApiController]
+    public class BasketController : ControllerBase
+    {
+        [HttpGet]
+        [CustomAuthorize]
+        public async Task<List<DishBasketDTO>> GetDishInBasket()
+        {
+            var userId = JWTTokenHelper.GetUserIdFromToken(HttpContext);
+
+            throw new NotImplementedException();
+        }
+
+
+        [HttpPost("/dish/{dishId}")]
+        [CustomAuthorize]
+        public async Task PutDishInBasket(Guid dishId)
+        {
+            var userId = JWTTokenHelper.GetUserIdFromToken(HttpContext);
+
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete("/dish/{dishId}")]
+        [CustomAuthorize]
+        public async Task DeletDishFromBasket(Guid dishId, [FromQuery]bool increase = false)
+        {
+            var userId = JWTTokenHelper.GetUserIdFromToken(HttpContext);
+
+            throw new NotImplementedException();
+        }
+    }
+}
